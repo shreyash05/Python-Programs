703f3cafc01f130d7e07c9a2287c4d2e47b17894
Shreyash Gade <shreeyashgade@gmail.com>
2021-04-17 18:42:54 +0530

    Delete File Handeling directory

--- File Handeling/Demo3.py
+++
@@ -1,8 +0,0 @@
-def main():
-	name  = input("Enter the name of file:")
-	fobj = open(name,"r")		#create new file
-	size = int(input("Enter the number of elements to read"))
-	print("Data from file is:")
-	print(fobj.read(size))
-if __name__ == '__main__':
-		main()	
