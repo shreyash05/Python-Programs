a15ffc17133bdca7c9edd15203b05086e2bd55fb
Shreyash Gade <shreeyashgade@gmail.com>
2021-04-20 12:00:25 +0530

    Delete File_Handeling directory

--- File_Handeling/FileHandeling/Demo2.py
+++
@@ -1,7 +0,0 @@
-def main():
-	name  = input("Enter the name of file:")
-	fobj = open(name,"r")		#create new file
-	print("Data from file is")
-	print(fobj.read())
-if __name__ == '__main__':
-		main()	
--- File_Handeling/FileHandeling/Demo5.py
+++
@@ -1,8 +0,0 @@
-def main():
-	name  = input("Enter the name of file:")
-	fobj = open(name,"r")		#create new file
-	print("Data from file is:")
-	for Data in fobj:
-		print(Data)
-if __name__ == '__main__':
-		main()	
